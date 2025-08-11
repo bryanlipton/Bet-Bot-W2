3a399203a00058190654ee38b67989aca427fd37
bryanlipton <71773141+bryanlipton@users.noreply.github.com>
2025-08-11 17:04:46 -0400

    Update daily-pick.js

--- api/daily-pick.js
+++ api/daily-pick.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Content-Type', 'application/json');
-  
+   
   // Handle OPTIONS request
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
