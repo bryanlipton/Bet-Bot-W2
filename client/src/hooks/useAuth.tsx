9d021ec0c7351586d95ea8bb4b5f0f20e7667288
bryanlipton <71773141+bryanlipton@users.noreply.github.com>
2025-08-26 19:48:44 -0400

    Update useAuth.tsx

--- client/src/hooks/useAuth.tsx
+++ client/src/hooks/useAuth.tsx
@@ -72,7 +72,7 @@ const signInWithGoogle = async () => {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: window.location.origin,
+      redirectTo: 'https://bet-bot-w2.vercel.app',
       queryParams: {
         access_type: 'offline',
         prompt: 'consent',
